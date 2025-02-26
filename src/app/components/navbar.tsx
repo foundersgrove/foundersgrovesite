8ecd7b2b4e67a010c25953c6f1ebfb516ed38247
Sam McDonald <Sammcdonald828@gmail.com>
2025-02-26 00:08:34 -0500

    Home page navbar and header on home page

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,78 @@
+export default function NavBar() {
+	return (
+		<div>
+			{/* <nav className="fixed top-0 left-0 w-full z-50 bg-white/20 backdrop-blur-lg shadow-md">
+				<div className="max-w-5xl mx-auto px-4">
+					<div className="flex items-center justify-between h-16">
+						<span className="text-2xl text-gray-900 font-semibold">Logo</span>
+						<div className="flex space-x-4 text-gray-900">
+							<a href="#">Dashboard</a>
+							<a href="#">About</a>
+							<a href="#">Projects</a>
+							<a href="#">Contact</a>
+						</div>
+					</div>
+				</div>
+			</nav> */}
+			<div className="navbar fixed top-0 left-0 w-full z-50 bg-foreground/20 backdrop-blur-lg shadow-xl">
+				<div className="navbar-start">
+					<div className="dropdown">
+						<div
+							tabIndex={0}
+							role="button"
+							className="btn btn-ghost hover:glass text-background lg:hidden"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth="2"
+									d="M4 6h16M4 12h8m-8 6h16"
+								/>
+							</svg>
+						</div>
+						<ul
+							tabIndex={0}
+							className="menu menu-md rounded-b-md dropdown-content bg-background/90 backdrop-filter backdrop-blur-lg shadow-xl z-[50] mt-3 w-52 p-2 "
+						>
+							<li>
+								<a>Home</a>
+							</li>
+							<li>
+								<a>Our Work</a>
+							</li>
+							<li>
+								<a>Our Team</a>
+							</li>
+						</ul>
+					</div>
+					<a className="btn btn-ghost hover:glass text-xl text-background">
+						Founders Grove
+					</a>
+				</div>
+				<div className="navbar-center hidden lg:flex">
+					<ul className="menu menu-horizontal px-1">
+						<li>
+							<a>Home</a>
+						</li>
+						<li>
+							<a>Our Work</a>
+						</li>
+						<li>
+							<a>Our Team</a>
+						</li>
+					</ul>
+				</div>
+				<div className="navbar-end">
+					{/* <a className="btn btn-ghost hover:glass btn-md">Contact</a> */}
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,37 @@
 import Image from "next/image";
+import NavBar from "./components/navbar";
 
 export default function Home() {
 	//surrounding div old classes className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
 	// main old classes className="flex flex-col gap-8 row-start-2 items-center sm:items-start"
 	return (
-		<div className="">
-			<main className="">
-				<div className="wrapper">
-					<header>
-						<img src="/imgs/RedwoodBackground.jpg" alt="" className="back" />
-						<img
-							src="/imgs/RedwoodSingle.png"
-							alt="Redwood Tree Single"
-							className="fore"
-						/>
-						<h1 className="title text-center">Founders Grove</h1>
-					</header>
-					<section>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-						mollitia aspernatur at ad accusamus neque doloribus praesentium ut
-						numquam repellendus. Sapiente repellat odit cupiditate expedita,
-						quae consequuntur minima ad magnam!
-					</section>
-				</div>
+		<div>
+			<main className="wrapper">
+				<header>
+					<img
+						src="/imgs/RedwoodBackground.jpg"
+						alt="background image"
+						className="back"
+					/>
+					<img
+						src="/imgs/RedwoodSingle.png"
+						alt="Redwood Tree Single"
+						className="fore"
+					/>
+					<div className="font-arial bg-background/80 rounded-2xl mt-20 p-20 size-fit backdrop-filter backdrop-blur-lg shadow-2xl text-center">
+						<h1 className="title text-center font-arial font-semibold">
+							Founders Grove
+						</h1>
+						<p>Lets Bring Something to Life</p>
+					</div>
+				</header>
+
+				<section>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+					mollitia aspernatur at ad accusamus neque doloribus praesentium ut
+					numquam repellendus. Sapiente repellat odit cupiditate expedita, quae
+					consequuntur minima ad magnam!
+				</section>
 			</main>
 		</div>
 	);
