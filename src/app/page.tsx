640c2b04c24a4f0c3c5f9dd7698e951fea16cbbc
Sam McDonald <Sammcdonald828@gmail.com>
2025-02-27 15:26:46 -0500

    home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,30 +18,30 @@ export default function Home() {
 						alt="Redwood Tree Single"
 						className="fore"
 					/>
-					<div className="font-arial text-background bg-foreground/20 rounded-2xl p-20 size-fit backdrop-filter backdrop-blur-lg shadow-2xl text-center">
+					<div className="font-arial text-background bg-foreground/20 rounded-2xl p-20 size-fit backdrop-filter backdrop-blur-lg shadow-3xl text-center z-20">
 						<h1 className=" text-3xl text-center font-arial font-semibold">
 							Founders Grove
 						</h1>
 						<p>Lets Bring Something to Life</p>
 					</div>
 				</header>
-				<div className="space-y-24">
+				<div className="space-y-24 mx-4">
 					<div className="homecard space-y-5 font-arial max-w-4xl mx-auto text-background bg-foreground/20 rounded-2xl p-12 backdrop-filter backdrop-blur-lg shadow-2xl text-center">
 						<h2 className=" text-2xl text-center font-arial font-semibold">
 							What do we do?
 						</h2>
-						<p>
+						<p className="text-xl font-normal font-arial">
 							We are a team of developers, designers, and project managers that
 							are dedicated to bringing your ideas to life. We specialize in
 							creating websites and web applications that are tailored to your
 							needs.
 						</p>
 					</div>
-					<div className="homecard max-w-4xl space-y-5 font-arial mx-auto text-background bg-foreground/20 rounded-2xl p-12 backdrop-filter backdrop-blur-lg shadow-2xl text-center">
+					<div className="homecard max-w-4xl space-y-5 font-arial justify-center mx-auto text-background bg-foreground/20 rounded-2xl p-12 backdrop-filter backdrop-blur-lg shadow-2xl text-center">
 						<h2 className=" text-2xl text-center font-arial font-semibold">
 							What have we built?
 						</h2>
-						<p>
+						<p className="text-xl font-normal font-arial">
 							We have built a variety of websites and web applications for a
 							diverse range of clients. Our projects include e-commerce sites,
 							blogs, and custom web applications.
@@ -57,7 +57,7 @@ export default function Home() {
 						<h2 className=" text-2xl text-center font-arial font-semibold">
 							Why Us?
 						</h2>
-						<p>
+						<p className="text-xl font-normal font-arial">
 							We are a team of experienced professionals who are passionate
 							about creating high-quality websites and web applications. We are
 							committed to providing our clients with the best possible service
