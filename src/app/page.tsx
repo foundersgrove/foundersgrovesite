19c58fbb1827bae39c3d5f3a84cb01d6b68953f6
Sam McDonald <Sammcdonald828@gmail.com>
2025-04-12 21:24:31 -0700

    New Title card for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import HomeCard from "./components/homeCard";
 import { homeCards } from "@/config/homeCards";
+import TitleCard from "./components/titleCard";
 
 export default function Home() {
 	return (
 		<div className="relative flex flex-col min-h-screen">
 			{/* Background Image */}
 			<div className="back absolute inset-0 bg-[url('/imgs/RedwoodBackground.jpg')] bg-cover bg-center bg-no-repeat"></div>
-			
+
 			{/* Main Content */}
 			<div className="relative z-10 flex-grow pt-24">
 				<div className="mx-8 flex flex-col gap-96 py-40">
+					<TitleCard />
 					{homeCards.map((card, index) => (
 						<HomeCard
 							key={index}
