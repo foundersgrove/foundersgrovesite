46656e8903a91d5d894d6b20fcfc5c23a7afc191
Sam McDonald <Sammcdonald828@gmail.com>
2025-04-16 20:06:34 -0400

    footer color transparent on home

--- src/app/components/globalFooter.tsx
+++ src/app/components/globalFooter.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { useRef } from "react";
 import CalendlyEmbed from "./CalendlyEmbed";
+import { usePathname } from "next/navigation";
 
-export default function Footer() {
+export default function Footer({}) {
 	const modalRef = useRef<HTMLDialogElement>(null);
-	
+	const pathname = usePathname();
+	const transparent = pathname === "/";
+
 	const handleClick = () => {
 		if (modalRef.current) {
 			modalRef.current?.showModal();
@@ -13,14 +16,20 @@ export default function Footer() {
 
 	return (
 		<>
-			<footer className="relative mt-auto">
+			<footer
+				className={`relative mt-auto ${
+					transparent ? "" : "bg-black/90"
+				} text-background`}
+			>
 				<div className="max-w-4xl mx-auto p-8">
 					<div className="mb-8 flex justify-center">
-						<div 
-							className="p-12 bg-[#739E71] hover:bg-[#557A53] text-white rounded-xl cursor-pointer transition-all duration-150 text-center shadow-[8px_8px_0px_0px_#557A53] hover:shadow-[12px_12px_0px_0px_#3C5A3A] hover:translate-x-[-4px] hover:translate-y-[-4px]" 
+						<div
+							className="p-12 bg-[#739E71] hover:bg-[#557A53] text-white rounded-xl cursor-pointer transition-all duration-150 text-center shadow-[8px_8px_0px_0px_#557A53] hover:shadow-[12px_12px_0px_0px_#3C5A3A] hover:translate-x-[-4px] hover:translate-y-[-4px]"
 							onClick={handleClick}
 						>
-							<span className="text-[3rem] leading-none font-heading font-black tracking-wide">Tell us about your project</span>
+							<span className="text-[3rem] leading-none font-heading font-black tracking-wide">
+								Tell us about your project
+							</span>
 						</div>
 					</div>
 					<div className="border-t border-neutral-500 pt-6 text-center text-sm">
@@ -29,15 +38,13 @@ export default function Footer() {
 				</div>
 			</footer>
 
-			<dialog 
-				id="my_modal_2" 
-				className="modal" 
-				ref={modalRef}
-			>
+			<dialog id="my_modal_2" className="modal" ref={modalRef}>
 				<div className="modal-box max-w-3xl w-full p-0 bg-white">
 					<CalendlyEmbed url="https://calendly.com/dayne-foundersgrove/30min?hide_event_type_details=1&hide_gdpr_banner=1" />
 					<form method="dialog" className="absolute right-4 top-4 z-50">
-						<button className="btn btn-sm btn-circle bg-foreground/10 hover:bg-foreground/20 border-none text-foreground">✕</button>
+						<button className="btn btn-sm btn-circle bg-foreground/10 hover:bg-foreground/20 border-none text-foreground">
+							✕
+						</button>
 					</form>
 				</div>
 				<form method="dialog" className="modal-backdrop">
