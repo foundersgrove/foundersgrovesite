7efc7525ec56cc39a48ea13de75124e157b8a3f9
Sam McDonald <Sammcdonald828@gmail.com>
2025-04-21 21:24:38 -0400

    design fixes to home page

--- src/app/components/titleCard.tsx
+++ src/app/components/titleCard.tsx
@@ -1,12 +1,12 @@
 export default function TitleCard({}) {
 	return (
 		<div
-			className="homecard max-w-2xl w-full h-full font-arial mx-auto text-background bg-foreground/20 
+			className="homecard max-w-3xl w-full h-full font-arial mx-auto text-background bg-foreground/20 
 						rounded-xl p-10 backdrop-filter backdrop-blur-lg shadow-lg shadow-black/50 hover:shadow-black 
 						transition-all duration-300 text-center"
 		>
 			{/* <h2 className="text-3xl font-arial font-semibold mb-4">Founders Grove</h2> */}
-			<h2 className="main text-center text-background mx-auto font-normal font-arial">
+			<h2 className="text-2xl font-semibold text-center text-background mx-auto font-arial">
 				Lets Build Your
 			</h2>
 			<p className="life" aria-label="Masterpiece">
